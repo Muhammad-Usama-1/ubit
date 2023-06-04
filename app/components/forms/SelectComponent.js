d0af70cef1a04596bcb6d04d3c8712378427793d
Muhammad Usama <mdusama225@gmail.com>
2023-06-04 15:09:57 +0500

    create select feild in creaate job

--- app/components/forms/SelectComponent.js
+++ app/components/forms/SelectComponent.js
@@ -1,49 +1,71 @@
 import React, { useState, useRef, useEffect } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
 import { useFormikContext } from "formik";
+import SelectDropdown from "react-native-select-dropdown";
+const countries = ["Egypt", "Canada", "Australia", "Ireland"];
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { color } from "../../config/colors";
+// import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons"; // Assuming you have the necessary dependencies installed
+import ErrorMsg from "./ErrorMsg";
 
-const SelectComponent = ({ name, items }) => {
-  const { values, setFieldValue } = useFormikContext();
-  const [isOpen, setIsOpen] = useState(false);
-  const controllerRef = useRef(null);
-
-  useEffect(() => {
-    if (controllerRef.current) {
-      controllerRef.current.reset();
-    }
-  }, [values[name]]);
-
+const SelectComponent = ({ name, items, style }) => {
+  const { handleChange, handleBlur, values, errors, touched, setFieldValue } =
+    useFormikContext();
+  // const { values, setFieldValue } = useFormikContext();
   const handleValueChange = (itemValue) => {
     setFieldValue(name, itemValue);
   };
-  const checkFunc = () => {
-    console.log("ckicked..");
-  };
-
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-
+  const renderDropdownIcon = () => (
+    <TouchableOpacity onPress={() => console.log("Dropdown icon pressed")}>
+      <MaterialIcons name="arrow-drop-down" size={24} color="black" />
+    </TouchableOpacity>
+  );
   return (
-    <DropDownPicker
-      items={items}
-      controller={(instance) => (controllerRef.current = instance)}
-      defaultValue={values[name]}
-      containerStyle={{ height: 40, marginVertical: 10 }}
-      style={{ backgroundColor: "#fafafa" }}
-      itemStyle={{ justifyContent: "flex-start" }}
-      dropDownStyle={{ backgroundColor: "#fafafa" }}
-      onChangeItem={(item) => checkFunc(item.value)}
-      onOpen={handleOpen}
-      onClose={handleClose}
-      open={isOpen}
-      //   placeholder="Select an option"
-    />
+    <View style={styles.container}>
+      <SelectDropdown
+        renderDropdownIcon={renderDropdownIcon}
+        data={items}
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+          handleValueChange(selectedItem);
+        }}
+        defaultButtonText={"Select country"}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          // text represented after item is selected
+          // if data array is an array of objects then return selectedItem.property to render after item is selected
+          return selectedItem;
+        }}
+        rowTextForSelection={(item, index) => {
+          // text represented for each item in dropdown
+          // if data array is an array of objects then return item.property to represent item in dropdown
+          return item;
+        }}
+        // renderDropdownIcon=
+        buttonStyle={{
+          width: "100%",
+          backgroundColor: "#FFFFFF",
+          borderRadius: 20,
+        }}
+        dropDownStyle={{ backgroundColor: color.primary }} // Set the dropDownStyle to change the dropdown options background color
+        dropdownIconPosition="right"
+        // buttonStyle={{ width: "100%" }} // Set the buttonStyle to make the dropdown full width
+        buttonTextStyle={{ color: color.primary }} // Set the buttonTextStyle to change the text color
+        // rowStyle={{ backgroundColor: color.primary }}
+      />
+      <ErrorMsg error={errors[name]} visible={touched[name]} />
+    </View>
   );
 };
 
 export default SelectComponent;
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: color.primary,
+    width: "100%", // Set the container width to 100% for full width
+  },
+  dropdownStyle: {
+    // backgroundColor: color.primary, // Change the background color of the dropdown
+  },
+});
