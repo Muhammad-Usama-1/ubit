67d28f8f8149ddc1525058dc8acb3ee17b3e470d
Muhammad Usama <mdusama225@gmail.com>
2023-06-12 23:59:28 +0500

    change background color

--- app/screens/JobListingScreen.js
+++ app/screens/JobListingScreen.js
@@ -169,7 +169,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   JobListingScreenContainer: {
-    backgroundColor: color.danger,
+    backgroundColor: color.primary,
 
     paddingHorizontal: 5,
     flex: 1,
