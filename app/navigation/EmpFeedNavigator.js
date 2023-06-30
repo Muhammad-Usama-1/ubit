fef7c1bd21230e47d3c66b6c9d2b1de1e578f73d
Muhammad Usama <mdusama225@gmail.com>
2023-06-30 08:33:58 +0500

    created  new navigator for employe

---
+++ app/navigation/EmpFeedNavigator.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// import ListingsScreen from "../screens/ListingsScreen";
+// import ListingDetailsScreen from "../screens/ListingDetailsScreen";
+// import ExploreScreen from "../screens/ExploreScreen";
+// import MessagesScreen from "../screens/MessagesScreen";
+import JobListingScreen from "../screens/JobListingScreen";
+import JobDetails from "../screens/JobDetails";
+import ApplyToJobScreen from "../screens/ApplyToJobScreen";
+import AvailableJobScreen from "../screens/AvailableJobScreen";
+import ApplySuccess from "../screens/AppliedSuccessScreen";
+import StudentListingScreen from "../screens/StudentListingScreen";
+
+const Stack = createNativeStackNavigator();
+
+const EmpFeedNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <Stack.Screen name="students" component={StudentListingScreen} />
+  </Stack.Navigator>
+);
+export default EmpFeedNavigator;
