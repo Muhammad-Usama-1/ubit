5e22f34c792d9511e36b0c0f141d6fdfab609db4
Muhammad Usama <mdusama225@gmail.com>
2023-09-08 20:13:48 +0500

    created screen for emplyer for displyaing thier   posted job

---
+++ app/screens/EmployeePostedJobScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const EmployeePostedJobScreen = () => {
+  return (
+    <View>
+      <Text>EmployeePostedJobScreen</Text>
+    </View>
+  );
+};
+
+export default EmployeePostedJobScreen;
+
+const styles = StyleSheet.create({});
