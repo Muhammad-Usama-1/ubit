b913a9e845bf526b33c19b2cd7893acab1944db2
Muhammad Usama <mdusama225@gmail.com>
2023-09-12 01:20:11 +0500

    beutify job card for employer

--- app/components/JobCard.js
+++ app/components/JobCard.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { color } from "../config/colors";
 
-const JobCard = ({ jobTitle, onPress }) => {
+const JobCard = ({ jobTitle, onPress, job }) => {
+  const formatISODateToHumanReadable = (isoDate) => {
+    const date = new Date(isoDate);
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    const datePart = date.toLocaleDateString("en-US", options);
+    // const timePart = date.toLocaleTimeString("en-US");
+
+    return `${datePart} `;
+  };
+  console.log(job);
+  const formattedDate = formatISODateToHumanReadable(job.postedAt);
+
   return (
     <TouchableOpacity onPress={onPress}>
       <View
@@ -9,11 +21,14 @@ const JobCard = ({ jobTitle, onPress }) => {
           padding: 16,
           borderWidth: 1,
           borderColor: "#ccc",
+          backgroundColor: color.white,
           marginBottom: 16,
           borderRadius: 8,
+          marginHorizontal: 10,
         }}
       >
         <Text style={{ fontSize: 18 }}>{jobTitle}</Text>
+        <Text style={{ fontSize: 18 }}>Posted On {formattedDate}</Text>
       </View>
     </TouchableOpacity>
   );
